6563ad23f55d08aef2e8b349e0711bd9be3fea5b
Efrén Juárez-Castillo <46583863+juarezefren@users.noreply.github.com>
2024-01-15 17:33:35 -0600

    Add files via upload

--- MatrizRotacion.java
+++ MatrizRotacion.java
@@ -9,46 +9,67 @@ package com.mycompany.matrizrotacion;
  * @author juare
  */
 public class MatrizRotacion {
-
     public static void main(String[] args) {
         int[][] original = {
             {7,12,9,21}, 
             {8,5,48,6,1},
             {13,4,37,27}
-        };
+        };        
         
         int[][] rotada = rotarMatriz90Grados(original);
+        int[][] reflejada = reflejarMatrizHorizontalmente(original);
+        int suma = calcularSuma(original);
 
         System.out.println("Matriz Original:");
         imprimirMatriz(original);
-        
+        System.out.println("Suma de la Matriz Original: " + suma);
         System.out.println("Matriz Rotada:");
         imprimirMatriz(rotada);
-        
-        
+        System.out.println("Matriz Reflejada Horizontalmente:");
+        imprimirMatriz(reflejada);                
     }
     
-    public static int[][] rotarMatriz90Grados(int[][] matriz) {
-    int filas = matriz.length;
-    int columnas = matriz[0].length;
-    int[][] rotada = new int[columnas][filas];
-
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            rotada[j][filas - 1 - i] = matriz[i][j];
+    public static int[][] rotarMatriz90Grados(int[][] matriz) {        
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        int[][] rotada = new int[columnas][filas];
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                rotada[j][filas - 1 - i] = matriz[i][j];
+            }
         }
+        return rotada;
     }
-    return rotada;
-}
-
-public static void imprimirMatriz(int[][] matriz) {
-    for (int[] fila : matriz) {
-        for (int elemento : fila) {
-            System.out.print(elemento + "\t");
+    
+    public static int calcularSuma(int[][] matriz) {
+        int suma = 0;        
+        for (int[] fila : matriz) {
+            for (int elemento : fila) {
+                suma += elemento;
+            }
         }
-        System.out.println();
+        return suma;
     }
-}
-    
-    
+                        
+    public static int[][] reflejarMatrizHorizontalmente(int[][] matriz) {
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        int[][] reflejada = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                reflejada[i][j] = matriz[filas - 1 - i][j];
+            }
+        }
+        return reflejada;
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int[] fila : matriz) {
+            for (int elemento : fila) {
+                System.out.print(elemento + "\t");
+            }
+            System.out.println();
+        }
+    }        
 }
